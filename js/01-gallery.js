edbc3e52b3af9128cb2b660ab00c4e04b628fe99
Volodymyr <vovkul@gmail.com>
2023-09-28 00:58:05 +0200

    add_HW7_1_without_escape_button

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+// console.dir(gallery);
+
+gallery.insertAdjacentHTML("afterbegin", createMarkUp(galleryItems));
+gallery.addEventListener("click", handlerClick);
+
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</li>
+  `
+    )
+    .join("");
+}
+
+function handlerClick(evt) {
+  evt.preventDefault();
+  const currentImage = evt.target.dataset.source;
+  console.dir(currentImage);
+  const instance = basicLightbox.create(`
+    <img src="${currentImage}" width="800" height="600">
+`);
+
+  instance.show();
+}
