9eefc67984978343a02f0bc46bce386ad5764c76
Volodymyr <vovkul@gmail.com>
2023-09-28 23:00:01 +0200

    add hw7_task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+// console.dir(gallery);
+
+gallery.insertAdjacentHTML("afterbegin", createMarkUp(galleryItems));
+// gallery.addEventListener("click", handlerClick);
+
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+       </li>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
